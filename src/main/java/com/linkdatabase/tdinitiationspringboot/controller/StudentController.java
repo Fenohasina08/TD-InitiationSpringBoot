f43742336cac9d6107badf38810f935966eb7667
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-19 17:19:42 +0300

    feat: getwelcome work

--- src/main/java/com/linkdatabase/tdinitiationspringboot/controller/StudentController.java
+++ src/main/java/com/linkdatabase/tdinitiationspringboot/controller/StudentController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
     @GetMapping("/welcome")
     public String welcome(@RequestParam String name) {
-        return  name;
+        return  "Welcome " + name;
     }
 }
