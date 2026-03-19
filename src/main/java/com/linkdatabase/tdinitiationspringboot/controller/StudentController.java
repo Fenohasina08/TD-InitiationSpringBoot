8605d9a9e44e29d9495e336b3d65dc319390d537
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-19 20:49:20 +0300

    feat: question B ok

--- src/main/java/com/linkdatabase/tdinitiationspringboot/controller/StudentController.java
+++ src/main/java/com/linkdatabase/tdinitiationspringboot/controller/StudentController.java
@@ -1,14 +1,28 @@
 package com.linkdatabase.tdinitiationspringboot.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import com.linkdatabase.tdinitiationspringboot.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @RestController
 public class StudentController {
+    private List<Student> listStudent = new ArrayList<>();
+
     @GetMapping("/welcome")
     public String welcome(@RequestParam String name) {
         return  "Welcome " + name;
+    }
+
+    @PostMapping("/students")
+    public String  createStudent(@RequestBody List<Student> students) {
+        listStudent.addAll(students);
+        return listStudent.stream()
+                .map(s -> s.getFirstName() + " " + s.getLastName())
+                .collect(Collectors.joining(", "));
     }
 
 
--- src/main/java/com/linkdatabase/tdinitiationspringboot/model/Student.java
+++ src/main/java/com/linkdatabase/tdinitiationspringboot/model/Student.java
@@ -1,4 +1,16 @@
 package com.linkdatabase.tdinitiationspringboot.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Student {
+    private String Reference;
+    private String FirstName ;
+    private String LastName ;
+    private Integer Age ;
 }
