bfa51888e381cc6e575362661a2e83a9045ab20b
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-19 17:02:04 +0300

    feat: creation de dossier controller

---
+++ src/main/java/com/linkdatabase/tdinitiationspringboot/TdInitiationSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.linkdatabase.tdinitiationspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdInitiationSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdInitiationSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linkdatabase/tdinitiationspringboot/TdInitiationSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkdatabase.tdinitiationspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdInitiationSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
